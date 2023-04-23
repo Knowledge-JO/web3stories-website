8439953b04febcbf1c435e9cbe474b519155b7ac
Knowledge-JO <megamindtheincredible@gmail.com>
2023-04-23 01:56:37 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -13,15 +13,16 @@ window.addEventListener('load', () => {
 })
 
 open_bar.addEventListener('click', () => {
-    if(menu_v.classList.contains('remove-menu-overlay')){
-        menu_v.classList.remove('remove-menu-overlay')
-    }
+    // if(menu_v.classList.contains('remove-menu-overlay')){
+    //     menu_v.classList.remove('remove-menu-overlay')
+    // }
     menu_v.style.transform = "translateY(0px)"
-    menu_v.style.transition = '1s'
+    menu_v.style.transition = '2s'
 })
 
 close_bar.addEventListener('click', () => {
-    menu_v.classList.add('remove-menu-overlay')
-    
+    // menu_v.classList.add('remove-menu-overlay')
+    menu_v.style.transform = "translateY(-5000px)"
+    menu_v.style.transition = '5s'
 })
 
